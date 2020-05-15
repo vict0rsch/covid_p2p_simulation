b674e25fe57275d4d9d50a480ced01fdceec1198
vict0rsch <vsch@pm.me>
2020-05-14 22:28:39 -0400

    refactor

--- tests/test_covid_testing.py
+++ tests/test_covid_testing.py
@@ -31,6 +31,10 @@ def print_dict(title, dic, is_sorted=None):
     )
 
 
+def date_str(date):
+    return str(date).split()[0]
+
+
 def compute_positivity_rate(positive_tests_per_day, negative_tests_per_day, average=3):
     rates = []
     days = []
@@ -51,61 +55,81 @@ def compute_positivity_rate(positive_tests_per_day, negative_tests_per_day, aver
 
 if __name__ == "__main__":
     import warnings
+
     warnings.filterwarnings("ignore")
     # https://coronavirus.jhu.edu/testing/testing-positivity
     # https://www.canada.ca/content/dam/phac-aspc/documents/services/diseases/2019-novel-coronavirus-infection/surv-covid19-epi-update-eng.pdf
     path = Path(__file__).parent
-    ExpConfig.load_config(path / "test_configs" / "test_covid_test.yml") # test_covid_test = no intervention
+    ExpConfig.load_config(
+        path / "test_configs" / "test_covid_test.yml"
+    )  # test_covid_test = no intervention
     outfile = None
+
+    # ----------------------------
+    # -----  Run Simulation  -----
+    # ----------------------------
     n_people = 1000
     simulation_days = 30
     init_percent_sick = 0.01
+    start_time = datetime.datetime(2020, 2, 28, 0, 0)
     monitors, tracker = simulate(
         n_people=n_people,
-        start_time=datetime.datetime(2020, 2, 28, 0, 0),
+        start_time=start_time,
         simulation_days=simulation_days,
         outfile=outfile,
         init_percent_sick=init_percent_sick,
         out_chunk_size=500,
     )
 
+    # ---------------------------
+    # -----  Retreive Data  -----
+    # ---------------------------
     data = extract_tracker_data(tracker, ExpConfig)
-
-    cum_R = np.cumsum(data["r"])
-
     tm = data["test_monitor"]
     days = [
-        str(datetime.datetime(2020, 2, 28, 0, 0) + datetime.timedelta(days=i)).split()[
-            0
-        ]
+        date_str(start_time + datetime.timedelta(days=i))
         for i in range(simulation_days)
     ]
 
+    # -------------------------
+    # -----  Daily Stats  -----
+    # -------------------------
+    cum_R = np.cumsum(data["r"])
+    i_for_day = {days[i]: d for i, d in enumerate(data["i"])}
+
+    # ------------------------------
+    # -----  Positivity Rates  -----
+    # ------------------------------
     positive_tests_per_day = {d: 0 for d in days}
     negative_tests_per_day = {d: 0 for d in days}
     for m in tm:
         if m["result"] == "positive":
-            positive_tests_per_day[str(m["timestamp"]).split()[0]] += 1
+            positive_tests_per_day[date_str(m["timestamp"])] += 1
         if m["result"] == "negative":
-            negative_tests_per_day[str(m["timestamp"]).split()[0]] += 1
-
+            negative_tests_per_day[date_str(m["timestamp"])] += 1
     tests_per_human = Counter([m["name"] for m in tm])
-    tests_per_day = Counter([str(m["timestamp"]).split()[0] for m in tm])
+    tests_per_day = Counter([date_str(m["timestamp"]) for m in tm])
     positivity_rates = compute_positivity_rate(
         positive_tests_per_day, negative_tests_per_day, average=3
     )
-    people_with_several_tests = {k: v for k, v in tests_per_human.items() if v > 1}
+    positives = len([m for m in tm if m["result"] == "positive"])
+    negatives = len([m for m in tm if m["result"] == "negative"])
+    positive_rate = positives / (negatives + positives)
 
+    # ----------------------------
+    # -----  Multiple Tests  -----
+    # ----------------------------
+    people_with_several_tests = {k: v for k, v in tests_per_human.items() if v > 1}
     multi_test_dates = {}
     for m in tm:
         if m["name"] in people_with_several_tests:
             old = multi_test_dates.get(m["name"], [])
-            new = "{} : {}".format(str(m["timestamp"]).split()[0], m["result"])
+            new = "{} : {}".format(date_str(m["timestamp"]), m["result"])
             multi_test_dates[m["name"]] = old + [new]
 
-    positives = len([m for m in tm if m["result"] == "positive"])
-    negatives = len([m for m in tm if m["result"] == "negative"])
-    positive_rate = positives / (negatives + positives)
+    # ----------------------
+    # -----  Symptoms  -----
+    # ----------------------
     symptoms = Counter([s for m in tm for s in m["symptoms"]])
     symptoms_positive = Counter(
         [s for m in tm for s in m["symptoms"] if m["result"] == "positive"]
@@ -114,18 +138,19 @@ if __name__ == "__main__":
         [s for m in tm for s in m["symptoms"] if m["result"] == "negative"]
     )
 
-    i_for_day = {days[i]: d for i, d in enumerate(data["i"])}
-
+    # --------------------
+    # -----  Prints  -----
+    # --------------------
+    print("\n" + "-" * 50 + "\n" + "-" * 50)
     print("Cumulative removed per day: ", cum_R)
     print("Test events: ", len(tm))
     print("Individuals: ", len(set(m["name"] for m in tm)))
-    # print_dict("Multi-tests humans", multi_test_dates)
     print_dict("Tests per day", tests_per_day)
     print(
         "Results last day ( N | P | P / (N+P) ): ", negatives, positives, positive_rate
     )
     print("For reference, May 14th in Canada: ", 1104855, 66536, 66536 / 1172796)
+    print_dict("Positivity rates", positivity_rates)
     print_dict("All symptoms", symptoms, is_sorted="desc")
     print_dict("Symptoms when positive tests", symptoms_positive, is_sorted="desc")
     print_dict("Symptoms when negative tests", symptoms_negative, is_sorted="desc")
-    print_dict("Positivity rates", positivity_rates)
